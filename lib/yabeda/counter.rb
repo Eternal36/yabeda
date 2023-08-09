6794c0e369980cee74da80618b01f6e3ae7a2daf
Eternal36 <33805342+Eternal36@users.noreply.github.com>
2023-08-09 17:04:39 +0300

    Update counter.rb

--- lib/yabeda/counter.rb
+++ lib/yabeda/counter.rb
@@ -3,7 +3,7 @@
 module Yabeda
   # Growing-only counter
   class Counter < Metric
-    def increment(tags, by: 1)
+    def increment(by: 1, **tags)
       all_tags = ::Yabeda::Tags.build(tags, group)
       values[all_tags] += by
       ::Yabeda.adapters.each do |_, adapter|
